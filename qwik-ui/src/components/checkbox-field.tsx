9e74ae2be058ab87034121caf8b572e82b781b47
Erik Jensen <365211+erikrj@users.noreply.github.com>
2026-01-26 20:46:24 -0700

    Added id to CheckboxField

--- qwik-ui/src/components/checkbox-field.tsx
+++ qwik-ui/src/components/checkbox-field.tsx
@@ -1,6 +1,7 @@
 import {component$} from '@builder.io/qwik';
 
 export interface CheckboxFieldProps {
+  id?: string;
   label: string;
   name?: string;
   checked?: boolean;
@@ -9,10 +10,11 @@ export interface CheckboxFieldProps {
 export default component$((props: CheckboxFieldProps) => {
   return (
     <div class="fieldset">
-      <label class="label">
+      <label class="label" {...(props.id && {for: props.id})}>
         <input
           type="checkbox"
           {...(props.name && {name: props.name})}
+          {...(props.id && {id: props.id})}
           {...(props.checked && {checked: props.checked})}
           class="checkbox"
         />
