5fb70d480f1ea29302b045ccffac575407ef2257
Erik Jensen <365211+erikrj@users.noreply.github.com>
2026-01-27 18:13:18 -0700

    Added onClick to AddButton

--- qwik-ui/src/components/add-button.tsx
+++ qwik-ui/src/components/add-button.tsx
@@ -1,13 +1,14 @@
-import {component$, Slot} from '@builder.io/qwik';
+import {component$, QRL, Slot} from '@builder.io/qwik';
 import {MdiAddCircleOutline} from '@nr1e/qwik-icons';
 
 export interface AddButtonProps {
   class?: string;
+  onClick$?: QRL<(event: Event) => void>;
 }
 
 export const AddButton = component$((props: AddButtonProps) => {
   return (
-    <button class={`btn ${props.class ?? ''}`}>
+    <button class={`btn ${props.class ?? ''}`} onClick$={props.onClick$}>
       <MdiAddCircleOutline size={24} />
       <Slot />
     </button>
