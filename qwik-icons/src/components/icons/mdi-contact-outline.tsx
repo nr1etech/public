f8ea11a04e468034a7705e054c3ed22826f53a06
Erik Jensen <365211+erikrj@users.noreply.github.com>
2026-02-11 10:56:01 -0700

    Added more MDI icons

---
+++ qwik-icons/src/components/icons/mdi-contact-outline.tsx
@@ -0,0 +1,13 @@
+import {component$} from '@builder.io/qwik';
+import {IconProps, Svg} from '../svg';
+
+export const MdiContactOutline = component$((props: IconProps) => {
+  return (
+    <Svg {...props} viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M19 19H5V5h14m0-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2m-2.5 13.25c0-1.5-3-2.25-4.5-2.25s-4.5.75-4.5 2.25V17h9M12 12.25A2.25 2.25 0 0 0 14.25 10A2.25 2.25 0 0 0 12 7.75A2.25 2.25 0 0 0 9.75 10A2.25 2.25 0 0 0 12 12.25"
+      />
+    </Svg>
+  );
+});
